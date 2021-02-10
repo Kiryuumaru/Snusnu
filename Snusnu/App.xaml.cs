7ed3f8d0a0f32d6fd5523da284adee6c4ca0ce28
Kiryuumaru <kiryuumaru@outlook.com>
2021-02-10 14:30:27 +0800

    Added license

--- Snusnu/App.xaml.cs
+++ Snusnu/App.xaml.cs
@@ -13,5 +13,9 @@ namespace Snusnu
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mzk2ODI0QDMxMzgyZTM0MmUzMEMvQWpQK05DdjFVb0lsSDhqTWtjMUNra3B1c3hJNFhqVzljTGxYVDE1djA9");
+        }
     }
 }
